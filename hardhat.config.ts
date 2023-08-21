60313bdfd598fb0ea77f4d61925899eac3da3c06
JinyuChen <fsyv.me@gmail.com>
2023-08-21 16:57:36 +0800

    Updating solc config follows： 1. evm-version：byzantium; 2. gas limit: OxFFFFFFF; 3. gas price 1; 4. cliche period 1.

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,7 +2,37 @@ import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.12",
+  defaultNetwork: "hardhat",
+  solidity: {
+    version: "0.8.12",
+    // Optional
+    settings:
+    {
+      // Optional: Optimizer settings
+      "optimizer": {
+        // Disabled by default.
+        // NOTE: enabled=false still leaves some optimizations on. See comments below.
+        // WARNING: Before version 0.8.6 omitting the 'enabled' key was not equivalent to setting
+        // it to false and would actually disable all the optimizations.
+        "enabled": true,
+      },
+      // Version of the EVM to compile for.
+      // Affects type checking and code generation. Can be homestead,
+      // tangerineWhistle, spuriousDragon, byzantium, constantinople, petersburg, istanbul or berlin
+      "evmVersion": "byzantium",
+    }
+  },
+  networks: {
+    hardhat: {
+      blockGasLimit: 0xfffffff,
+      gasPrice: 1,
+      initialBaseFeePerGas: 1,
+      gas: 0xffffff,
+    }
+  },
+  gasReporter: {
+    enabled: true,
+  }
 };
 
 export default config;
