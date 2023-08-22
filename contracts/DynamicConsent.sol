d37754899be082669a94854c490b91988b16aa76
xiaolin9595 <linaacc9595@gmail.com>
2023-08-22 17:17:18 +0800

    up-to-date 0822

--- contracts/DynamicConsent.sol
+++ contracts/DynamicConsent.sol
@@ -30,7 +30,7 @@ contract DynamicConsent {
      *	regardless of studyID.
      */
     int256 private constant WILDCARD = -1;
-
+    uint256  MaxStudyID;
     // studyID=> patientID  => Patient 数据
     mapping(uint256 => mapping(uint256 => Consent[] )) public dataBase;
     // Patient  patient;
@@ -66,6 +66,7 @@ contract DynamicConsent {
         string[] calldata _patientCategoryChoices,
         string[] calldata _patientElementChoices
     ) public {
+        if(_studyID>MaxStudyID)MaxStudyID=_studyID;
         // 获取Patient对象的引用，避免重复的mapping查找操作
         Consent[] storage patient1 = dataBase[_studyID][_patientID];
         // 创建新的Consent对象并推入数组
@@ -247,15 +248,16 @@ contract DynamicConsent {
         bytes32[] memory categoryNeeded,
         bytes32 catogorysInDatabase
     ) public pure returns (bool yes) {
-        bytes32 category0;
-        bytes32 category1;
+        bytes32 category;
         bool exist=false;
+        bytes32 zero;
         for (uint n = 0; n < categoryNeeded.length; n++) {
             for (uint k = 0; k < 32; k += 2) {
-            category0 = bytes32(catogorysInDatabase[k]);
-            category1 = (bytes32(catogorysInDatabase[k + 1]) >> 8) | category0;
-                if (category1 == categoryNeeded[n]) {
+            category = bytes32(catogorysInDatabase[k]);
+            if(category==zero)break;
+                if (category == categoryNeeded[n]) {
                    exist=true;
+                   break;
                 }
             }
             if (exist==false){
@@ -268,15 +270,16 @@ contract DynamicConsent {
         bytes32[] memory categoryNeeded,
         bytes32 catogorysInDatabase
     ) public pure returns (uint256) {
-        bytes32 category0;
-        bytes32 category1;
+        bytes32 category;
         uint256 counterOfSatisfied=0;
+        bytes32 zero;
         for (uint n = 0; n < categoryNeeded.length; n++) {
             for (uint k = 0; k < 32; k += 2) {
-            category0 = bytes32(catogorysInDatabase[k]);
-            category1 = (bytes32(catogorysInDatabase[k + 1]) >> 8) | category0;
-                if (category1 == categoryNeeded[n]) {
+            category = bytes32(catogorysInDatabase[k]);
+            if(category==zero)break;
+                if (category == categoryNeeded[n]) {
                    counterOfSatisfied+=1;
+                   break;
                 }
             }
         }
@@ -289,13 +292,15 @@ contract DynamicConsent {
         bytes32 element0;
         bytes32 element1;
         uint256 counterOfSatisfied=0;
+        bytes32 zero;
         for (uint h = 0; h < 32; h += 2) {
             element0 = bytes32(elementsInDatabase[h]);
             element1 = (bytes32(elementsInDatabase[h + 1]) >> 8) | element0;
-            //element=getBytes32FromElements(elementsInDatabase,h*2);
+            if(element1==zero)break;
             for (uint g = 0; g < categoryNeeded.length; g++) {
                 if (element1 == categoryNeeded[g]) {
                      counterOfSatisfied+=1;
+                     break;
                 }
             }
         }
@@ -362,7 +367,7 @@ contract DynamicConsent {
             shortnumber = getPrefixBytes32forCategory(
                 categorySharingChoices[i]
             );
-            result |= shortnumber >> (i * 16);
+            result |= shortnumber >> (i * 8);
             q = bytes(extendCategorys[shortnumber]);
             if (q.length == 0) {
                 extendCategorys[shortnumber] = categorySharingChoices[i];
@@ -409,31 +414,20 @@ contract DynamicConsent {
         return bytes32((b1 << 248) | (b3 << 240));
     }
 
-    function getPrefixfor(bytes32 b) public pure returns (bytes32[] memory) {
-        bytes32 b1 = 0;
-        bytes32 b2 = 0;
-        bytes32[] memory a = new bytes32[](16);
-        for (uint i = 0; i < 16; i += 2) {
-            // 取出第一个数字
-            b1 = bytes32(b[i]);
-            //取出第二个数字
-            b2 = bytes32(b[i + 1]);
-            a[i] = b1 | (b2 >> 8);
-        }
-        return a;
-    }
+
 
     //注：uint256(0x01)<<2 | uint256(0x04) = 0x104 = 260  bytes32(260）=0x0000000000000000000000000000000000000000000000000000000000000104
     function getPrefixBytes32forCategory(
         string memory s
     ) public pure returns (bytes32) {
         bytes memory b = bytes(s);
-        bytes32 b1 = 0;
-        bytes32 b2 = 0;
+        uint b1 = 0;
+        uint b2 = 0;
         // 取出第一个数字
-        b1 = bytes32(b[0]);
-        b2 = bytes32(b[1]);
-        return b1 | (b2 >> 8);
+        b1 = uint8(b[0])-48;
+        b2 = uint8(b[1])-48;
+        b1 = b1*10+b2;
+        return bytes32((b1 << 248));
     }
 
     //插入patientID到patientsOfStudy中
@@ -464,20 +458,14 @@ contract DynamicConsent {
         int256 _startTime,
         int256 _endTime
     ) public view returns (string memory output) {
-        require(_patientID >= 0, "Invalid patientID");
         if (_startTime == -1) {
             _startTime = 0;
         }
         if (_endTime == -1) {
             _endTime = 9999999999;
         }
-
-        bool foundConsent = false; // Flag to track if any consent was found
-
-        require(_startTime <= _endTime, "Invalid time input");
-
         if (_studyID == -1) {
-            for (uint256 i = 0; i <= 100; i++) {
+            for (uint256 i = 0; i <= MaxStudyID; i++) {
                 if (dataBase[i][_patientID].length > 0) {
                         Consent[] memory patient2 = dataBase[i][_patientID];
 
@@ -493,7 +481,7 @@ contract DynamicConsent {
                                     getConsentString(i, patient2[j])
                                 )
                             );
-                            foundConsent = true;
+                       
                         }
                     }
                 }
@@ -517,13 +505,9 @@ contract DynamicConsent {
                                 )
                             )
                         );
-                        foundConsent = true;
                     }
                 }
             }
-        }
-        if (!foundConsent) {
-            return "No matching consents found.";
         }
         return output;
     }
@@ -578,11 +562,12 @@ contract DynamicConsent {
         bytes32[] memory a;
         string memory result;
         string memory finallyresult;
-        a = getPrefixfor(input);
+        bytes32 zero;
+        a = getPrefixforCategory(input);
 
         for (uint256 i = 0; i < a.length; i++) {
-            if (a[i] == 0) {
-                continue;
+            if (a[i] == zero) {
+                break;
             }
             result = extendCategorys[a[i]];
             if (i == 0) {
@@ -602,12 +587,9 @@ contract DynamicConsent {
         bytes32[] memory a;
         string memory result;
         string memory finallyresult;
-        a = getPrefixfor(input);
+        a = getPrefixforElement(input);
 
         for (uint256 i = 0; i < a.length; i++) {
-            if (a[i] == 0) {
-                continue;
-            }
             result = extendElements[a[i]];
             if (i == 0) {
                 finallyresult = result;
@@ -619,15 +601,66 @@ contract DynamicConsent {
         }
         return finallyresult;
     }
-
-    function getBytes32FromElements(
-        bytes32 b,
-        uint start
-    ) internal pure returns (bytes32) {
-        bytes32 result = 0;
-        for (uint i = start; i < start + 2; i++) {
-            result |= bytes32(b[i]) >> ((i - start) * 8);
+    function getPrefixforElement(bytes32 b) public pure returns (bytes32[] memory) {
+        bytes32 b1 = 0;
+        bytes32 b2 = 0;
+        uint256 counter;
+        bytes32 zero;
+        uint256 j;
+        bytes32[] memory a = new bytes32[](16);
+        for (uint i = 0; i < 16; i += 2) {
+            // 取出第一个数字
+            b1 = bytes32(b[i]);
+            //取出第二个数字
+            b2 = bytes32(b[i + 1]);
+            j=i/2;
+            a[j] = b1 | (b2 >> 8);
+            if(a[j]==zero){
+                counter=j;
+                break;
+            }
         }
-        return result;
+        assembly {
+            // 获取原数组的长度位置
+            let oldLengthPtr := a
+            // 修改长度值
+            mstore(oldLengthPtr, counter)
+        }
+        return a;
     }
+    //     function getPrefixforElement(bytes32 b) public pure returns (bytes32[] memory) {
+    //     bytes32 b1 = 0;
+    //     bytes32 b2 = 0;
+    //     bytes32[] memory a = new bytes32[](16);
+    //     for (uint i = 0; i < 16; i += 2) {
+    //         // 取出第一个数字
+    //         b1 = bytes32(b[i]);
+    //         //取出第二个数字
+    //         b2 = bytes32(b[i + 1]);
+    //         a[i] = b1 | (b2 >> 8);
+    //     }
+    //     return a;
+    // }
+
+    function getPrefixforCategory(bytes32 b) public pure returns (bytes32[] memory) {
+        bytes32[] memory a = new bytes32[](32);
+        uint256 counter;
+        bytes32 zero;
+        for (uint i = 0; i < 32; i ++) {
+            // 取出第一个数字
+            a[i] = bytes32(b[i]) ;
+            if(a[i]==zero){
+                counter=i;
+                break;
+            }
+        }
+        assembly {
+            // 获取原数组的长度位置
+            let oldLengthPtr := a
+            // 修改长度值
+            mstore(oldLengthPtr, counter)
+        }
+        return a;
+    }
+
 }
