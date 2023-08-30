cb376c5c5d3c0ff283e154d4cb6c93684452ae34
fsyv <fsyv.me@gmail.com>
2023-08-30 14:44:13 +0800

    queryForResearcher测试脚本

--- scripts/DynamicConsent.ts
+++ scripts/DynamicConsent.ts
@@ -1,92 +1,96 @@
 
 
-// // 数据库
-// const dataBase: Record<string, any> = {};
+type Consent = {
+  recordTime: number;
+  patientCategoryChoices: string[];
+  patientElementChoices: string[];
+};
 
-// // 存储函数
-// function storeRecord(key: string, value: any): void {
-//   // 将数据存储到数据库中
-//   dataBase[key] = value;
-//   console.log(`成功将数据存储到数据库：Key: ${key}, Value: ${value}`);
-// }
+// studyID : patientID  : Consent[]
+const dataBase: Record<number, Record<number, Consent[]>> = {};
 
-// // 查询函数
-// function queryForResearcher(key: string): any {
-//   // 从数据库中查询数据
-//   const data = dataBase[key];
-//   if (data) {
-//     console.log(`查询到了与 ${key} 相关的数据：${data}`);
-//     return data;
-//   } else {
-//     console.log(`未找到与 ${key} 相关的数据`);
-//     return null;
-//   }
-// }
-
-// // 查询函数
-// function queryForPatient(key: string): any {
-//     // 从数据库中查询数据
-//     const data = dataBase[key];
-//     if (data) {
-//       console.log(`查询到了与 ${key} 相关的数据：${data}`);
-//       return data;
-//     } else {
-//       console.log(`未找到与 ${key} 相关的数据`);
-//       return null;
-//     }
-//   }
-
-
-  type Consent = {
-    recordTime: number;
-    patientCategoryChoices: string[];
-    patientElementChoices: string[];
-  };
-  
-  const dataBase: Record<number, Record<number, Consent[]>> = {};
-  
-  function storeRecord(
-    _patientID: number,
-    _studyID: number,
-    _recordTime: number,
-    _patientCategoryChoices: string[],
-    _patientElementChoices: string[]
-  ): void {
-    // const record: Record = {
-    //   patientID: _patientID,
-    //   studyID: _studyID,
-    //   recordTime: _recordTime,
-    //   patientCategoryChoices: _patientCategoryChoices,
-    //   patientElementChoices: _patientElementChoices,
-    // };
-
-    const consent: Consent = {
-        recordTime: _recordTime,
-        patientCategoryChoices: _patientCategoryChoices,
-        patientElementChoices: _patientElementChoices,
-    }
-  
-    dataBase[_patientID][_studyID].push(consent)
+export function storeRecord(
+  _patientID: number,
+  _studyID: number,
+  _recordTime: number,
+  _patientCategoryChoices: string[],
+  _patientElementChoices: string[]
+): void {
+  const consent: Consent = {
+    recordTime: _recordTime,
+    patientCategoryChoices: _patientCategoryChoices,
+    patientElementChoices: _patientElementChoices,
   }
-  
-//   function queryForResearcher(
-//     _studyID: number,
-//     _endTime: number,
-//     _requestedCategoryChoices: string[],
-//     _requestedElementChoices: string[]
-//   ): Record[] {
-//     const filteredRecords = dataBase.filter((record) => {
-//       return (
-//         record.studyID === _studyID &&
-//         record.recordTime <= _endTime &&
-//         arraysContain(record.patientCategoryChoices, _requestedCategoryChoices) &&
-//         arraysContain(record.patientElementChoices, _requestedElementChoices)
-//       );
-//     });
-  
-//     return filteredRecords;
-//   }
-  
-//   function arraysContain(arr1: string[], arr2: string[]): boolean {
-//     return arr2.every((item) => arr1.includes(item));
-//   }
+
+  if(!dataBase[_studyID])
+  {
+    dataBase[_studyID] = {};
+  }
+
+  if(!dataBase[_studyID][_patientID])
+  {
+    dataBase[_studyID][_patientID] = [];
+  }
+
+  dataBase[_studyID][_patientID].push(consent)
+}
+
+export function queryForResearcher(
+  _studyID: number,
+  _endTime: number,
+  _requestedCategoryChoices: string[],
+  _requestedElementChoices: string[]
+): string[] {
+
+  let patientIDs: string[] = [];
+  const study = dataBase[_studyID];
+
+  for (const patientID in study) {
+
+    if (study.hasOwnProperty(patientID)) {
+
+      const consents = study[patientID];
+      for (let consent of consents) {
+        if(_endTime != -1 && consent.recordTime > _endTime)
+        {
+          break;
+        }
+
+        let categoryChoicesCount = 0;
+        for(let categoryChoices of consent.patientCategoryChoices)
+        {
+          if(!_requestedCategoryChoices.includes(categoryChoices))
+          {
+            break;
+          }
+
+          categoryChoicesCount++;
+        }
+
+        if(categoryChoicesCount != _requestedCategoryChoices.length)
+        {
+          break;
+        }
+
+        let elementSharingChoicesCount = 0;
+        for(let elementChoices of consent.patientElementChoices)
+        {
+          if(!_requestedElementChoices.includes(elementChoices))
+          {
+            break;
+          }
+
+          elementSharingChoicesCount++;
+        }
+        if(elementSharingChoicesCount != _requestedElementChoices.length)
+        {
+          break;
+        }
+
+        patientIDs.push(patientID)
+      }
+    }
+  }
+
+  return [...new Set(patientIDs)];
+}
---
+++ test/comparison.test.ts
@@ -0,0 +1,148 @@
+import {
+    time,
+    loadFixture,
+} from "@nomicfoundation/hardhat-network-helpers";
+import { anyValue } from "@nomicfoundation/hardhat-chai-matchers/withArgs";
+import { expect } from "chai";
+import { ethers } from "hardhat";
+import { DynamicConsentLast2 } from "../typechain-types";
+const fs = require("fs");
+import {storeRecord, queryForResearcher} from "../scripts/DynamicConsent"
+
+describe("DynamicConsent", function () {
+    let dynamicConsent: DynamicConsentLast2;
+
+    before(async function () {
+        // 部署合约
+        console.log("Deploying...");
+        dynamicConsent = await ethers.deployContract("DynamicConsentLast2");
+        await dynamicConsent.waitForDeployment();
+        console.log(
+            `Contract deployed to ${dynamicConsent.target}`
+        );
+
+        // 加载数据集
+        // let datasets: Array<string> = [
+        //     "./dataset/consents/1/training_data.json",
+        //     "./dataset/consents/2/training_data.json",
+        //     "./dataset/consents/3/training_data.json",
+        //     "./dataset/consents/4/training_data.json"
+        // ];
+
+        let datasets: Array<string> = [
+            "./dataset/consents/1/training_data.json"
+        ];
+
+
+        for (let dataset of datasets) {
+
+            console.log("Loading dataset: " + dataset);
+
+            let training_data = JSON.parse(fs.readFileSync(dataset));
+            // 存储数据
+            console.log("Runing storeRecord function...");
+
+            let counter = 0;
+            for (const i in training_data) {
+
+                if(counter > 50) break;
+                counter = counter + 1;
+
+                await dynamicConsent.storeRecord(
+                    training_data[i]["patientID"],
+                    training_data[i]["studyID"],
+                    training_data[i]["timestamp"],
+                    training_data[i]["categorySharingChoices"],
+                    training_data[i]["elementSharingChoices"]
+                );
+
+                // 测试脚本
+                storeRecord(
+                    training_data[i]["patientID"],
+                    training_data[i]["studyID"],
+                    training_data[i]["timestamp"],
+                    training_data[i]["categorySharingChoices"],
+                    training_data[i]["elementSharingChoices"]
+                );
+
+                console.log(i);
+            }
+
+            console.log(dataset + "_total: " + training_data.length);
+        }
+    })
+
+    // queryForResearcher
+    it("queryForResearcher", async function () {
+
+        //////////////////////////////////////////////
+        // 加载数据集
+        let datasets: Array<string> = [
+            "./dataset/queries/researcher_queries.json"
+        ];
+
+        for (var dataset of datasets) {
+            console.log("Load dataset: " + dataset);
+
+            let researcher_queries = JSON.parse(fs.readFileSync(dataset))
+            for (const i in researcher_queries) {
+
+                console.log("Input: " + 
+                    researcher_queries[i]["studyID"] + ", " + 
+                    researcher_queries[i]["timestamp"] + ", " + 
+                    researcher_queries[i]["categorySharingChoices"] + ", " + 
+                    researcher_queries[i]["elementSharingChoices"]
+                )
+
+                let result_solidity = await dynamicConsent.queryForResearcher(
+                    researcher_queries[i]["studyID"],
+                    researcher_queries[i]["timestamp"],
+                    researcher_queries[i]["categorySharingChoices"],
+                    researcher_queries[i]["elementSharingChoices"]
+                );
+
+                let result_ts = queryForResearcher(
+                    researcher_queries[i]["studyID"],
+                    researcher_queries[i]["timestamp"],
+                    researcher_queries[i]["categorySharingChoices"],
+                    researcher_queries[i]["elementSharingChoices"]
+                );
+
+
+                console.log("queryForResearcher_" + dataset + "_" + i + " solidity: " + result_solidity);
+                console.log("queryForResearcher_" + dataset + "_" + i + " hardhat : " + result_ts);
+            }
+        }
+    })
+
+    // // queryForPatient
+    // it("queryForPatient", async function () {
+
+    //     // 加载数据集
+    //     let datasets: Array<string> = [
+    //         "./dataset/queries/patient_queries.json"
+    //     ];
+
+    //     for (var dataset of datasets) {
+    //         console.log("Load dataset: " + dataset);
+
+    //         let researcher_queries = JSON.parse(fs.readFileSync(dataset))
+    //         for (const i in researcher_queries) {
+
+    //             console.time("queryForPatient: " + dataset + "_" + i)
+
+    //             let result = await dynamicConsent.queryForPatient(
+    //                 researcher_queries[i]["patientID"],
+    //                 researcher_queries[i]["studyID"],
+    //                 researcher_queries[i]["startTimestamp"],
+    //                 researcher_queries[i]["endTimestamp"]
+    //             )
+
+    //             console.timeEnd("queryForPatient: " + dataset + "_" + i)
+
+    //             console.log("queryForPatient" + dataset + "_" + i + ": " + result);
+    //         }
+    //     }
+    // })
+
+})
